d9edb53d95f33ea5b5dd2381f7e5aac99984e90a
Zhantore <753gg@mail.ru>
2020-09-12 13:31:50 +0600

    Аккордион

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,43 @@
+const accordion = (triggersSelector) => {
+    const btns = document.querySelectorAll(triggersSelector);
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            btns.forEach(button => {
+                if (!(btn == button)) {
+                    button.classList.remove('active-style');
+                    button.nextElementSibling.classList.remove('active-content');
+                    button.nextElementSibling.style.maxHeight = '0px';
+                }
+            });
+
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+
+            if (this.classList.contains('active-style')) {
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+            } else {
+                this.nextElementSibling.style.maxHeight = '0px';
+            }
+        });
+    });
+
+    // blocks = document.querySelectorAll(itemsSelector);
+
+    // blocks.forEach(block => {
+    //     block.classList.add('animated', 'fadeInDown');
+    // });
+
+    // btns.forEach(btn => {
+    //     btn.addEventListener('click', function () {
+    //         if (!this.classList.contains('active')) {
+    //             btns.forEach(btn => {
+    //                 btn.classList.remove('active', 'active-style');
+    //             });
+    //             this.classList.add('active', 'active-style');
+    //         }
+    //     });
+    // });
+};
+
+export default accordion;
